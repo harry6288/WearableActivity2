fd8d63958d0773c7fa4e700a6db11d1b1170d8e4
hardeepkaur <hardeepwalia019@gmail.com>
2019-06-27 21:41:00 -0400

    Initial Commit

---
+++ WearableActivity2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WearableActivity2
+//
+//  Created by hardeep kaur on 2019-06-27.
+//  Copyright © 2019 hardeep kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
