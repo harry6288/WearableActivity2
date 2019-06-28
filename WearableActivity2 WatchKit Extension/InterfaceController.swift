03153f0beb3c3df7885a15b275b0457adbe26546
hardeepkaur <hardeepwalia019@gmail.com>
2019-06-27 22:04:47 -0400

    R2----create label, button and session in watch side,           watch received message from phone side

--- WearableActivity2 WatchKit Extension/InterfaceController.swift
+++ WearableActivity2 WatchKit Extension/InterfaceController.swift
@@ -8,9 +8,18 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
+    
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+    }
+    
+    @IBOutlet weak var MessageLabel: WKInterfaceLabel!
+    
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -18,9 +27,24 @@ class InterfaceController: WKInterfaceController {
         // Configure interface objects here.
     }
     
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        // output a debug message to the terminal
+        print("WATCH: Got a message!")
+        
+        // update the message with a label
+        MessageLabel.setText("\(message)")
+
+    }
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        if WCSession.isSupported() {
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
+
     }
     
     override func didDeactivate() {
